1981c9eff3417ec4b36375495b9edd71096f6c9d
Александр Нужнов <samuray@cool.ru>
2020-08-26 10:13:35 +0300

    add docker

---
+++ docker-build/push_latest.ts
@@ -0,0 +1,19 @@
+import { spawn } from "child_process";
+import * as pkg from "./package.json";
+
+const docker_cmd = spawn("docker", ["push", `${pkg.docker_image}:latest`], {
+    cwd: __dirname
+});
+
+docker_cmd.stdout.on("data", (data) => {
+  console.log(`${data.toString().trim()}`);
+});
+
+docker_cmd.stderr.on("data", (data) => {
+  console.error(`${data.toString().trim()}`);
+  process.exit(1);
+});
+
+docker_cmd.on("error", (error) => {
+    console.log(error);
+});
