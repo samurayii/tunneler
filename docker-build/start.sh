a850b7147857fdbd92496e4b9bc045144021110b
Александр Нужнов <samuray@cool.ru>
2020-08-27 10:06:32 +0300

    fix

--- docker-build/start.sh
+++ docker-build/start.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/dumb-init /bin/bash
 
 if [[ $APP_RESTARTER ]]; then
-    app-restarter -e "${APP_NAME} --config /${APP_NAME}/config.toml" -tmp /app_restarter_tmp -c /${APP_NAME}
+    app-restarter -e "node app.js --config ./config.toml" -tmp /app_restarter_tmp -c /${APP_NAME}
 else
     cd /${APP_NAME}
-    ${APP_NAME} --config /${APP_NAME}/config.toml
+    node app.js --config ./config.toml
 fi
