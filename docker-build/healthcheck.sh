db46a8e0ecffb6b45f5f6ddb5e7eb22648d6813c
samurayii <48275898+samurayii@users.noreply.github.com>
2020-08-31 11:37:08 +0300

    Update healthcheck.sh

--- docker-build/healthcheck.sh
+++ docker-build/healthcheck.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ $CASH_ONLINE_DOCKER_HEALTHCHECK_ENABLE == "true" ]]; then
+if [[ $TEMPLATE_DOCKER_HEALTHCHECK_ENABLE == "true" ]]; then
     echo "start docker healthcheck"
     cd /${APP_NAME}
     node docker-healthcheck.js --config ./config.toml
