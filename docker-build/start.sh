b519256e4fa58e8b9a0d94d3584d24d2df7e98d1
Александр Нужнов <samuray@cool.ru>
2020-08-31 10:44:41 +0300

    add docker healthcheck

--- docker-build/start.sh
+++ docker-build/start.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/dumb-init /bin/bash
 
+if [[ $TEMPLATE_DOCKER_HEALTHCHECK_ENABLE == "true" ]]; then
+    echo "docker healthcheck activated"
+fi
+
 if [[ $APP_RESTARTER ]]; then
     app-restarter -e "node app.js --config ./config.toml" -tmp /app_restarter_tmp -c /${APP_NAME}
 else
